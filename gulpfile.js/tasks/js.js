4609c54a15c295d6699a8122d1d70ae16ba0073c
Leandro Kimura <leandro.kimura@mirumagency.com>
2017-07-06 13:42:20 -0300

    Fixing indentation.

--- gulpfile.js/tasks/js.js
+++ gulpfile.js/tasks/js.js
@@ -7,15 +7,15 @@ var uglify      = require('gulp-uglify');
 var concat      = require('gulp-concat');
 
 var paths = {
-    src: path.join(config.root.src, config.tasks.js.src, '/**/*.' + config.tasks.js.extensions),
-    dest: path.join(config.root.dest, config.tasks.js.dest)
+  src: path.join(config.root.src, config.tasks.js.src, '/**/*.' + config.tasks.js.extensions),
+  dest: path.join(config.root.dest, config.tasks.js.dest)
 };
 
 var jsTask = function () {
-    gulp.src(paths.src)                        // Arquivos que serão carregados, veja variável 'js' no início
-    .pipe(concat('script.min.js'))      // Arquivo único de saída
-    .pipe(uglify())                     // Transforma para formato ilegível
-    .pipe(gulp.dest(paths.dest));          // pasta de destino do arquivo(s)
+  gulp.src(paths.src)
+  .pipe(concat('script.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest(paths.dest));
 };
 
 gulp.task('js', jsTask);
